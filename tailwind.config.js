87376bf8bb7dc3ed3f2d12824fc8df9a1bd1e996
junbin-tan <junbin.tan@u.nus.edu>
2023-07-15 13:21:28 +0800

    yep

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ['"PT Sans"', 'sans-serif'],
+      },
+      boxShadow: {
+        'dark-lg': '0 10px 15px -3px rgba(0, 0, 0, 0.3), 0 4px 6px -2px rgba(0, 0, 0, 0.2)',
+      },
+    },
+  },
+  plugins: [],
+}
